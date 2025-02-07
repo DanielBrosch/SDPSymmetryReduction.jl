2910603d81a1afb916c93d6767230fbe7c2ff082
Marek Kaluba <kalmar@amu.edu.pl>
2025-02-07 15:05:57 +0100

    disable qap tests on Windows
    readdlm doesn't play well with qapdata

--- test/qap.jl
+++ test/qap.jl
@@ -10,22 +10,24 @@ function read_qapdata(file_name, T::Type)
     return A, B
 end
 
-@testset "qap problem: esc16j" begin
-    esc16_file = joinpath(@__DIR__, "qapdata", "esc16j.dat")
+@static if !Sys.IsWindows()
+    @testset "qap problem: esc16j" begin
+        esc16_file = joinpath(@__DIR__, "qapdata", "esc16j.dat")
 
-    A, B = read_qapdata(esc16_file, Float64)
-    CAb = QuadraticAssignment(A, B)
-    P = SDPSymmetryReduction.admissible_subspace(CAb..., verbose=true)
-    @test SDPSymmetryReduction.dim(P) == 150
+        A, B = read_qapdata(esc16_file, Float64)
+        CAb = QuadraticAssignment(A, B)
+        P = SDPSymmetryReduction.admissible_subspace(CAb..., verbose=true)
+        @test SDPSymmetryReduction.dim(P) == 150
 
-    Q_hat = SDPSymmetryReduction.diagonalize(Float64, P, verbose=true)
-    @test sort(size.(Q_hat, 2)) == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7]
+        Q_hat = SDPSymmetryReduction.diagonalize(Float64, P, verbose=true)
+        @test sort(size.(Q_hat, 2)) == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7]
 
-    model = opt_model(P, Q_hat, CAb)
-    JuMP.set_optimizer(model, CSDP.Optimizer)
-    JuMP.set_silent(model)
-    JuMP.optimize!(model)
+        model = opt_model(P, Q_hat, CAb)
+        JuMP.set_optimizer(model, CSDP.Optimizer)
+        JuMP.set_silent(model)
+        JuMP.optimize!(model)
 
-    @test JuMP.termination_status(model) == JuMP.OPTIMAL
-    @test JuMP.objective_value(model) ≈ 7.7942186 rtol = 1e-7
+        @test JuMP.termination_status(model) == JuMP.OPTIMAL
+        @test JuMP.objective_value(model) ≈ 7.7942186 rtol = 1e-7
+    end
 end
